5bed98660a33a336439aec30638dfed43d5a9728
LeonGeroge <leongeroge@gmail.com>
2019-10-13 23:27:10 +0800

    init

---
+++ src/main/java/com/practice/mall/platformadmin/MallPlatformAdminApplication.java
@@ -0,0 +1,15 @@
+package com.practice.mall.platformadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+//@EnableFeignClients
+@SpringBootApplication
+public class MallPlatformAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPlatformAdminApplication.class, args);
+    }
+
+}
